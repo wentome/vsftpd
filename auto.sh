a5f74f8f6245c0291eaa0db72f2a2596df7e77e5
cp <840157203@qq.com>
2017-04-03 00:38:27 +0800

    add files

--- auto.sh
+++ auto.sh
@@ -1,5 +1,43 @@
 #!/bin/sh
-
-#tar xvzf vsftpd-3.0.3.tar.gz
+if [ -x vsftpd-3.0.3 ]
+then
+    echo "unzip is done"
+else
+    echo  "unzip..."
+    tar xvzf vsftpd-3.0.3.tar.gz
+fi 
+cp Makefile vsftpd-3.0.3/
+cp vsftpd.conf /etc
 cd vsftpd-3.0.3
-ls
+#make clean 
+if [ -x /usr/local/man/man5 ]
+then
+    echo "man5 ok"
+else
+    echo "creat man5"
+    mkdir /usr/local/man/man5
+fi
+
+if [ -x /usr/local/man/man8 ]
+then
+    echo "man8 ok"
+else
+    echo "creat man8"
+    mkdir /usr/local/man/man8
+fi
+
+make && make install
+if [ ! -x /usr/share/empty ]
+then
+    mkdir /usr/share/empty
+fi
+
+if [ ! -x /var/ftp ]
+then
+    mkdir /var/ftp
+fi
+
+useradd -d /var/ftp ftp
+chown root.root /var/ftp
+chmod og-w /var/ftp
+
